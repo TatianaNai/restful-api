a0d414bd512563b73d599a2f0fbd9c5375f8e09a
TatianaNai <tatyana_tat333@mail.ru>
2025-09-15 12:54:26 +0300

    Добавлены модели для девайсов

---
+++ src/main/java/ru/restfulApi/models/DeviceModel.java
@@ -0,0 +1,12 @@
+package ru.restfulApi.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class DeviceModel {
+    private String id;
+    private String name;
+    private DeviceDataModel data;
+}
