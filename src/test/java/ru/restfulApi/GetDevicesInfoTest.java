5ac373da46bf259b05e3cec8a00467cfa52e8a93
TatianaNai <tatyana_tat333@mail.ru>
2025-09-15 14:34:11 +0300

    Использованы спецификации в тесте на получение девайсов

--- src/test/java/ru/restfulApi/GetDevicesInfoTest.java
+++ src/test/java/ru/restfulApi/GetDevicesInfoTest.java
@@ -1,34 +1,31 @@
 package ru.restfulApi;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import ru.restfulApi.endPoints.RestfulApiEndPoints;
-import ru.restfulApi.utils.Props;
+import ru.restfulApi.specifications.DefaultSpecification;
 
 public class GetDevicesInfoTest {
     @Test
     public void shouldHaveCorrectGetListDevices() {
         RestAssured.given()
-                .baseUri(Props.getProperty("baseUri"))
-                .contentType(ContentType.JSON)
+                .spec(DefaultSpecification.requestSpec())
                 .when()
                 .get(RestfulApiEndPoints.devices)
                 .then()
-                .statusCode(200);
+                .spec(DefaultSpecification.responceSpec());
     }
 
     @ValueSource(ints = {5, 4})
     @ParameterizedTest
     public void shouldHaveCorrectGetDeviceById(int id) {
         RestAssured.given()
-                .baseUri(Props.getProperty("baseUri"))
-                .contentType(ContentType.JSON)
+                .spec(DefaultSpecification.requestSpec())
                 .when()
                 .get(RestfulApiEndPoints.deviceById, id)
                 .then()
-                .statusCode(200);
+                .spec(DefaultSpecification.responceSpec());
     }
 }
