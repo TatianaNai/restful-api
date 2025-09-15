9d0bc5e1a326e696faa4ff93e8efe832afa5dc75
TatianaNai <tatyana_tat333@mail.ru>
2025-09-15 14:33:38 +0300

    Добавлены спецификации

---
+++ src/test/java/ru/restfulApi/specifications/DefaultSpecification.java
@@ -0,0 +1,26 @@
+package ru.restfulApi.specifications;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import ru.restfulApi.utils.Props;
+
+public class DefaultSpecification {
+    public static RequestSpecification requestSpec() {
+        return new RequestSpecBuilder()
+                .setBaseUri(Props.getProperty("baseUri"))
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.BODY)
+                .build();
+    }
+
+    public static ResponseSpecification responceSpec() {
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .log(LogDetail.BODY)
+                .build();
+    }
+}
