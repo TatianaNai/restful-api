bd1f08bc1363129805461b442088d31df39aace1
TatianaNai <tatyana_tat333@mail.ru>
2025-09-15 16:37:21 +0300

    Изменены модели для девайсов

--- src/main/java/ru/restfulApi/models/DeviceModel.java
+++ src/main/java/ru/restfulApi/models/DeviceModel.java
@@ -1,12 +1,16 @@
 package ru.restfulApi.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
-public class DeviceModel<T> {
+public class DeviceModel {
     private String id;
     private String name;
-    private T data;
+    private DeviceDataModel data;
 }
