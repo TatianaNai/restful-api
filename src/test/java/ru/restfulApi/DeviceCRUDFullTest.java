aed1ad9c72e684939f389dc3330f169702ff2783
TatianaNai <tatyana_tat333@mail.ru>
2025-09-16 12:24:04 +0300

    Добавлены тесты CRUD

---
+++ src/test/java/ru/restfulApi/DeviceCRUDFullTest.java
@@ -0,0 +1,103 @@
+package ru.restfulApi;
+
+import io.restassured.RestAssured;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.restfulApi.endPoints.RestfulApiEndPoints;
+import ru.restfulApi.models.DeviceDataModel;
+import ru.restfulApi.models.DeviceModel;
+import ru.restfulApi.services.DeviceRestService;
+import ru.restfulApi.specifications.DefaultSpecification;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@Slf4j
+public class DeviceCRUDFullTest extends BaseTest {
+    static Stream<Arguments> deviceProvider() {
+        return Stream.of(
+                Arguments.of(DeviceModel.builder()
+                                .name("Apple-test")
+                                .data(DeviceDataModel.builder()
+                                        .color("blue")
+                                        .price(33.33)
+                                        .capacityGB(2)
+                                        .build())
+                                .build(),
+                        DeviceModel.builder()
+                                .name("Apple-test2")
+                                .data(DeviceDataModel.builder()
+                                        .price(33.33)
+                                        .capacityGB(2)
+                                        .build())
+                                .build()),
+                Arguments.of(DeviceModel.builder()
+                                .name("Laptop-test")
+                                .data(DeviceDataModel.builder()
+                                        .year(1999)
+                                        .price(33.33)
+                                        .cpuModel("model")
+                                        .hardDiskSize("123")
+                                        .build())
+                                .build(),
+                        DeviceModel.builder()
+                                .name("Laptop-test")
+                                .data(DeviceDataModel.builder()
+                                        .year(1999)
+                                        .cpuModel("modelCPU")
+                                        .hardDiskSize("123")
+                                        .build())
+                                .build())
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("deviceProvider")
+    public void shouldHaveCorrectFullCRUDDevice(DeviceModel deviceToAdd, DeviceModel deviceToChange) {
+        String deviceId = RestAssured.given()
+                .spec(DefaultSpecification.requestSpec())
+                .when()
+                .body(deviceToAdd)
+                .post(RestfulApiEndPoints.devices)
+                .then()
+                .spec(DefaultSpecification.responseSpec(HttpStatus.SC_OK))
+                .extract()
+                .path("id");
+        log.info("Device with id \"{}\" is created", deviceId);
+
+        DeviceModel deviceBeforeChanging = DeviceRestService.getDeviceById(deviceId);
+
+        given()
+                .spec(DefaultSpecification.requestSpec())
+                .when()
+                .body(deviceToChange)
+                .put(RestfulApiEndPoints.deviceById, deviceId)
+                .then()
+                .spec(DefaultSpecification.responseSpec(HttpStatus.SC_OK));
+
+        DeviceModel deviceAfterChanging = DeviceRestService.getDeviceById(deviceId);
+
+        assertNotEquals(deviceBeforeChanging, deviceAfterChanging);
+
+        given()
+                .spec(DefaultSpecification.requestSpec())
+                .when()
+                .delete(RestfulApiEndPoints.deviceById, deviceId)
+                .then()
+                .spec(DefaultSpecification.responseSpec(HttpStatus.SC_OK));
+
+        given()
+                .spec(DefaultSpecification.requestSpec())
+                .when()
+                .get(RestfulApiEndPoints.deviceById, deviceId)
+                .then()
+                .spec(DefaultSpecification.responseSpec(HttpStatus.SC_NOT_FOUND));
+    }
+}
