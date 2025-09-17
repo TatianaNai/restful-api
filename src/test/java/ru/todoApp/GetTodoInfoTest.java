358a2bd4d201cfa7b04b4614716d4c7515af647b
TatianaNai <tatyana_tat333@mail.ru>
2025-09-17 19:09:08 +0300

    Добавлен тест shouldHaveCorrectGetTodoList

---
+++ src/test/java/ru/todoApp/GetTodoInfoTest.java
@@ -0,0 +1,11 @@
+package ru.todoApp;
+
+import org.junit.jupiter.api.Test;
+
+public class GetTodoInfoTest extends BaseTest{
+
+    @Test
+    public void shouldHaveCorrectGetTodoList() {
+
+    }
+}
