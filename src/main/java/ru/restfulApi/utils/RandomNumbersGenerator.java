cc902ea82e38167f954f40abecd25f982f14fb58
TatianaNai <tatyana_tat333@mail.ru>
2025-09-17 15:02:31 +0300

    Изменено название метода, генерирующего случайное число, на getRandomNumberInRange

--- src/main/java/ru/restfulApi/utils/RandomNumbersGenerator.java
+++ src/main/java/ru/restfulApi/utils/RandomNumbersGenerator.java
@@ -3,11 +3,11 @@ package ru.restfulApi.utils;
 import org.apache.commons.lang3.RandomUtils;
 
 public class RandomNumbersGenerator {
-    public static int numberGeneratorInRange(int minValue, int maxValue) {
+    public static int getRandomNumberInRange(int minValue, int maxValue) {
         return RandomUtils.nextInt(minValue, maxValue + 1);
     }
 
-    public static double numberGeneratorInRange(double minValue, double maxValue) {
+    public static double getRandomNumberInRange(double minValue, double maxValue) {
         return Math.round(RandomUtils.nextDouble(minValue, maxValue + 1) * 100.0) / 100.0;
     }
 }
