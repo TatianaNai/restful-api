9830f936369ae8df786f31b3fc1e8d56c2591bb2
TatianaNai <tatyana_tat333@mail.ru>
2025-09-17 19:07:33 +0300

    Добавлена генерация todo в предусловия тестов

--- src/test/java/ru/todoApp/BaseTest.java
+++ src/test/java/ru/todoApp/BaseTest.java
@@ -2,14 +2,42 @@ package ru.todoApp;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import ru.todoApp.models.TodoModel;
+import ru.todoApp.specifications.DefaultSpecification;
 import ru.todoApp.utils.Props;
+import ru.todoApp.utils.RandomGenerator;
+
+import static io.restassured.RestAssured.given;
 
 public abstract class BaseTest {
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = Props.getProperty("baseUri");
+        int amountOfTodo = given()
+                .spec(DefaultSpecification.requestSpec())
+                .when()
+                .get()
+                .jsonPath().getList("$").size();
+
+        if(amountOfTodo == 0) {
+            for(int i = 0; i < 10; i++) {
+                TodoModel todo = new TodoModel(RandomGenerator.getRandomLongId(),
+                        RandomGenerator.getRandomStringByLength(12),
+                        RandomGenerator.getRandomBoolean());
+
+                given()
+                        .spec(DefaultSpecification.requestSpec())
+                        .when()
+                        .body(todo)
+                        .post()
+                        .then()
+                        .spec(DefaultSpecification.responseSpec(HttpStatus.SC_CREATED));
+            }
+        }
+
     }
 
     @BeforeEach
