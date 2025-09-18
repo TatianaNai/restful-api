d190db62471ef4a27bdf8530445047ba56e1aeee
TatianaNai <tatyana_tat333@mail.ru>
2025-09-18 17:40:18 +0300

    Добавлено удаление todos после всех тестов

--- src/test/java/ru/todoApp/BaseTest.java
+++ src/test/java/ru/todoApp/BaseTest.java
@@ -3,45 +3,47 @@ package ru.todoApp;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import ru.todoApp.models.TodoModel;
-import ru.todoApp.specifications.DefaultSpecification;
+import ru.todoApp.services.TodoRestService;
 import ru.todoApp.utils.Props;
 import ru.todoApp.utils.RandomGenerator;
 
-import static io.restassured.RestAssured.given;
+import java.util.List;
+
 
 public abstract class BaseTest {
+
     @BeforeAll
     public static void setup() {
-        RestAssured.baseURI = Props.getProperty("baseUri");
-        int amountOfTodo = given()
-                .spec(DefaultSpecification.requestSpec())
-                .when()
-                .get()
-                .jsonPath().getList("$").size();
+        RestAssured.baseURI = Props.getStringProperty("baseUri");
+
+        TodoRestService todoRestService = new TodoRestService();
+        int amountOfTodo = todoRestService.getListByType("$", TodoModel.class,HttpStatus.SC_OK).size();
 
         if(amountOfTodo == 0) {
-            for(int i = 0; i < 10; i++) {
+            for(int i = 0; i < Props.getIntProperty("startAmountTodo"); i++) {
                 TodoModel todo = new TodoModel(RandomGenerator.getRandomLongId(),
-                        RandomGenerator.getRandomStringByLength(12),
+                        RandomGenerator.getRandomStringByLength(Props.getIntProperty("textLength")),
                         RandomGenerator.getRandomBoolean());
-
-                given()
-                        .spec(DefaultSpecification.requestSpec())
-                        .when()
-                        .body(todo)
-                        .post()
-                        .then()
-                        .spec(DefaultSpecification.responseSpec(HttpStatus.SC_CREATED));
+                todoRestService.post(todo, HttpStatus.SC_CREATED);
             }
         }
-
     }
 
     @BeforeEach
     public void setFilter() {
         RestAssured.filters(new AllureRestAssured());
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        TodoRestService todoRestService = new TodoRestService();
+        List<Long> ids = todoRestService.getListByType("id", Long.class,HttpStatus.SC_OK);
+        for(long id: ids) {
+            todoRestService.deleteById(id, HttpStatus.SC_NO_CONTENT);
+        }
     }
 }
