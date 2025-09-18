f56fe08ac009d7593b8bc872be4b7493b11cf562
TatianaNai <tatyana_tat333@mail.ru>
2025-09-18 19:12:29 +0300

    Добавлен тест контракта json на получение всех todos

---
+++ src/test/java/ru/todoApp/ContractTest.java
@@ -0,0 +1,22 @@
+package ru.todoApp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.todoApp.services.TodoRestService;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+@Slf4j
+public class ContractTest extends BaseTest {
+    private final TodoRestService todoRestService = new TodoRestService();
+
+    @Test
+    @DisplayName("Verify JSON schema for todo list")
+    public void shouldBeCorrectGetTodoResponseScheme() {
+        log.info("Check JSON contract");
+        todoRestService.getAllResponse(HttpStatus.SC_OK)
+                .body(matchesJsonSchemaInClasspath("todoResponseSchema.json"));
+    }
+}
