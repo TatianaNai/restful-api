9d2c8763be87dc72f08bde4c8e1d845c707d73e6
TatianaNai <tatyana_tat333@mail.ru>
2025-11-21 15:20:03 +0300

    Добавлены аннотации для сущности Todo

--- src/main/java/ru/todoapp/models/Todo.java
+++ src/main/java/ru/todoapp/models/Todo.java
@@ -1,15 +1,20 @@
 package ru.todoapp.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "todos")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
     private Boolean completed;
